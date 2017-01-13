b669f01c87af6874b60db062b24bb44ebbbb3539
mauq <mauqj3@gmail.com>
2017-01-12 23:19:28 -0800

    buttons work for returning column to be filtered

--- graph-node.js
+++ graph-node.js
@@ -96,49 +96,58 @@ d3.csv('test-energy.csv', function(error, data) {
   console.log(data);
   var json = formatData(data);
 
-  var colors = {
-        'environment':         '#edbd00',
-        'social':              '#367d85',
-        'animals':             '#97ba4c',
-        'health':              '#f5662b',
-        'research_ingredient': '#3f3e47',
-        'fallback':            '#9f9fa3'
-      };
-
-  var chart = d3.select("#chart").append("svg").chart("Sankey.Path");
-  chart
-    .name(label)
-    .colorNodes(function(name, node) {
-      return color(node, 1) || colors.fallback;
-    })
-    .colorLinks(function(link) {
-      return color(link.source, 4) || color(link.target, 1) || colors.fallback;
-    })
-    .nodeWidth(15)
-    .nodePadding(10)
-    .spread(true)
-    .iterations(0)
-    .draw(json);
-  function label(node) {
-    return node.name.replace(/\s*\(.*?\)$/, '');
-  }
-  function color(node, depth) {
-    var id = node.id.replace(/(_score)?(_\d+)?$/, '');
-    if (colors[id]) {
-      return colors[id];
-    } else if (depth > 0 && node.targetLinks && node.targetLinks.length == 1) {
-      return color(node.targetLinks[0].source, depth-1);
-    } else {
-      return null;
-    }
-  }
+  // var colors = {
+  //       'environment':         '#edbd00',
+  //       'social':              '#367d85',
+  //       'animals':             '#97ba4c',
+  //       'health':              '#f5662b',
+  //       'research_ingredient': '#3f3e47',
+  //       'fallback':            '#9f9fa3'
+  //     };
+  //
+  // var chart = d3.select("#chart").append("svg").chart("Sankey.Path");
+  // chart
+  //   .name(label)
+  //   .colorNodes(function(name, node) {
+  //     return color(node, 1) || colors.fallback;
+  //   })
+  //   .colorLinks(function(link) {
+  //     return color(link.source, 4) || color(link.target, 1) || colors.fallback;
+  //   })
+  //   .nodeWidth(15)
+  //   .nodePadding(10)
+  //   .spread(true)
+  //   .iterations(0)
+  //   .draw(json);
+  // function label(node) {
+  //   return node.name.replace(/\s*\(.*?\)$/, '');
+  // }
+  // function color(node, depth) {
+  //   var id = node.id.replace(/(_score)?(_\d+)?$/, '');
+  //   if (colors[id]) {
+  //     return colors[id];
+  //   } else if (depth > 0 && node.targetLinks && node.targetLinks.length == 1) {
+  //     return color(node.targetLinks[0].source, depth-1);
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
   //copy 'data' and filter each object to remove the key:value that corresponds to the column
-  function columnFilter() {
-
+  function columnFilter(val) {
+    console.log(val);
   }
 
   var button = document.getElementById('button');
+  var button2 = document.getElementById('button2');
 
-  button.addEventListener('click', columnFilter);
+  button.addEventListener('click', function() {
+    var val = this.value;
+    columnFilter(val);
+  });
+
+  button2.addEventListener('click', function() {
+    var val = this.value;
+    columnFilter(val);
+  })
 });
